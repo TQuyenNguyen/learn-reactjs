a5b85352f15e2ca9ec0a3e85df11b6d7999a4506
TQuyenNguyen <quyendn11062000@gmail.com>
2024-06-13 15:38:57 +0700

    style(Filter-by-Price): change UI

--- src/features/Product/components/Filters/FilterByPrice.jsx
+++ src/features/Product/components/Filters/FilterByPrice.jsx
@@ -1,12 +1,34 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Box, Button, TextField, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    padding: "16px",
+    borderTop: "solid 1px #dedede",
+  },
+  range: {
+    display: "flex",
+    flexFlow: "nowrap",
+    alignItems: "center",
+
+    marginBottom: "8px",
+    marginTop: "8px",
+
+    "& > span": {
+      marginLeft: "8px",
+      marginRight: "8px",
+    },
+  },
+}));
 
 FilterByPrice.propTypes = {
   onChange: PropTypes.func,
 };
 
 function FilterByPrice({ onChange }) {
+  const classes = useStyle();
   const [values, setValues] = useState({});
 
   const handleChange = (e) => {
@@ -29,17 +51,23 @@ function FilterByPrice({ onChange }) {
   };
 
   return (
-    <Box>
+    <Box className={classes.root}>
       <Typography variant="subtitle2">CHỌN KHOẢNG GIÁ</Typography>
 
-      <Box>
+      <Box className={classes.range}>
         <TextField
+          id="standard-basic"
+          label="From"
+          variant="standard"
           name="salePrice_gte"
           value={values.salePrice_gte}
           onChange={handleChange}
         />
         <span>-</span>
         <TextField
+          id="standard-basic"
+          label="To"
+          variant="standard"
           name="salePrice_lte"
           value={values.salePrice_lte}
           onChange={handleChange}
