e6cf636f4282c4198ccd091dee3b5199aacb88dc
TQuyenNguyen <quyendn11062000@gmail.com>
2024-06-13 14:51:19 +0700

    style(category-filter): change UI

--- src/features/Product/components/Filters/FilterByCategory.jsx
+++ src/features/Product/components/Filters/FilterByCategory.jsx
@@ -2,6 +2,28 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import categoryApi from "api/categoryApi";
 import { Box, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    padding: "16px",
+  },
+  menu: {
+    margin: 0,
+    padding: 0,
+    listStyleType: "none",
+
+    "& > li": {
+      marginTop: "8px",
+      transition: "all 0.25s",
+
+      "&:hover": {
+        color: "blue",
+        cursor: "pointer",
+      },
+    },
+  },
+}));
 
 FilterByCategory.propTypes = {
   onChange: PropTypes.func,
@@ -9,6 +31,7 @@ FilterByCategory.propTypes = {
 
 function FilterByCategory({ onChange }) {
   const [categoryList, setCategoryList] = useState([]);
+  const classes = useStyle();
 
   useEffect(() => {
     (async () => {
@@ -33,13 +56,13 @@ function FilterByCategory({ onChange }) {
   };
 
   return (
-    <Box>
+    <Box className={classes.root}>
       <Typography variant="subtitle2">DANH MỤC SẢN PHẨM</Typography>
 
-      <ul>
+      <ul className={classes.menu}>
         {categoryList.map((category) => (
           <li key={category.id} onClick={() => handleCategoryClick(category)}>
-            {category.name}
+            <Typography variant="body2">{category.name}</Typography>
           </li>
         ))}
       </ul>
