686c578a0c2203f3548d30b3eda67fb920779cbb
TQuyenNguyen <quyendn11062000@gmail.com>
2024-06-14 15:36:07 +0700

    feat(product-detail-page): render HTML on UI

--- src/features/Product/components/ProductDescription.jsx
+++ src/features/Product/components/ProductDescription.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import DOMPurify from "dompurify";
+import { Paper } from "@mui/material";
 
-ProductDescription.propTypes = {};
+ProductDescription.propTypes = {
+  product: PropTypes.object,
+};
 
-function ProductDescription(props) {
-  return <div>PD</div>;
+function ProductDescription({ product = {} }) {
+  const safeDescription = DOMPurify.sanitize(product.description);
+
+  return (
+    <Paper elevation={0} style={{ padding: "15px" }}>
+      <div dangerouslySetInnerHTML={{ __html: safeDescription }} />
+    </Paper>
+  );
 }
 
 export default ProductDescription;
