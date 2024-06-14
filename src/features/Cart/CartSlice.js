6e4279ac89b871215bc2c6f9d85dae485e39c905
TQuyenNguyen <quyendn11062000@gmail.com>
2024-06-14 16:56:14 +0700

    feat(Cart): add action for cart items

--- src/features/Cart/CartSlice.js
+++ src/features/Cart/CartSlice.js
@@ -14,6 +14,34 @@ const cartSlice = createSlice({
     hideMiniCart(state) {
       state.showMiniCart = false;
     },
+
+    addToCart(state, actions) {
+      //newItem = {id, product, quantity}
+      const newItem = actions.payload;
+      const index = state.cartItem.findIndex((x) => x.id === newItem.id);
+
+      if (index >= 0) {
+        //increase
+        state.cartItem[index].quantity += newItem.quantity;
+      } else {
+        //add to cart
+        state.cartItem.push(newItem);
+      }
+    },
+
+    setQuantity(state, actions) {
+      const { id, quantity } = actions.payload;
+      //check if product is available in cart
+      const index = state.cartItem.findIndex((x) => x.id === id);
+      if (index >= 0) {
+        state.cartItem[index].quantity = quantity;
+      }
+    },
+
+    removeFromCart(state, actions) {
+      const idNeedToRemove = actions.payload;
+      state.cartItem = state.cartItem.filter((x) => x.id !== idNeedToRemove);
+    },
   },
 });
 
